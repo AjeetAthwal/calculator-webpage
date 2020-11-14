a48c09d60d64af0d5c22e20f3ebd06539ee87d62
Ajeet Athwal <ajeet_athwal@hotmail.co.uk>
2020-11-14 22:51:05 +0000

    solved rounding going off screen

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ let displayValue = 0;
 let storedValue = 0;
 let currentOperator = "";
 let lastInput = "";
+let infinityError = false;
+
+const DP = 11;
+
+function getNumberLength(number){
+    return number.toString().replace(".","").length
+}
 
 function operate (operator, a, b){
     switch(operator){
@@ -29,12 +36,22 @@ function isOperator(input){
 }
 
 function addNumberToDisplay(e){
+    if (infinityError) document.querySelector(".calc-display").innerText = "LOL";
     if (lastInput === "equals") return;
+
+    let prevDisplayValue = displayValue;
+    let prevLastInput = lastInput;
     const pressedNumber = this.innerText;
     lastInput = pressedNumber;
     const displayHtml = document.querySelector(".calc-display");
     if (displayValue == 0) displayValue = pressedNumber;
     else displayValue += pressedNumber.toString();
+
+    if (getNumberLength(displayValue) > DP){
+        displayValue = prevDisplayValue;
+        lastInput  = prevLastInput;
+    }
+
     displayHtml.innerText = displayValue;
 }
 
@@ -44,15 +61,22 @@ function clearDisplay(e){
     displayValue = 0;
     storedValue = 0;
     currentOperator = "";
+    lastInput = "";
+    infinityError = false;
     document.querySelector(".calc-display").innerText = displayValue;
 }
 
 document.querySelector(".clear-btn").addEventListener("click", clearDisplay)
 
 function calcNumbers(e){
+    if (infinityError) return;
     if (!isOperator(lastInput)){
-        if (currentOperator !== "") storedValue = operate(currentOperator, storedValue, displayValue);
+        if (currentOperator !== "") storedValue = operate(currentOperator, storedValue, displayValue).toPrecision(DP);
         else storedValue = displayValue;
+
+        console.log(storedValue);
+        if (storedValue == Infinity) infinityError = true;
+
         // this code relies on the first class name of these divs being of the structure e.g divide <- this class MUST be first
         let operatorClass = this.parentElement.className;
         // keep only first class
@@ -60,19 +84,27 @@ function calcNumbers(e){
         lastInput = currentOperator;
         document.querySelector(".calc-display").innerText = storedValue;
         displayValue = 0;
+
+        if (infinityError) document.querySelector(".calc-display").innerText = "LOL";
     }
 }
 
 document.querySelectorAll(".operator-btn").forEach(btn => btn.addEventListener("click", calcNumbers));
 
 function calculateDisplay(e){
+    if (infinityError) return;
     if (lastInput === "equals") return;
     if (!isOperator(lastInput)){
-        if (currentOperator !== "") storedValue = operate(currentOperator, storedValue, displayValue);
+        if (currentOperator !== "") storedValue = operate(currentOperator, storedValue, displayValue).toPrecision(DP);
         else storedValue = displayValue;
+
+        if (storedValue == Infinity) infinityError = true;
+
         lastInput = "equals";
         document.querySelector(".calc-display").innerText = storedValue;
         displayValue = 0;
+
+        if (infinityError) document.querySelector(".calc-display").innerText = "LOL";
     }
 }
 
