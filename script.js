c82e432bbf7782bc4b1b524d28753843429de7e9
Ajeet Athwal <ajeet_athwal@hotmail.co.uk>
2020-11-14 23:44:26 +0000

    fixed error of operator being pressed after equals

--- script.js
+++ script.js
@@ -74,17 +74,19 @@ document.querySelector(".clear-btn").addEventListener("click", clearDisplay)
 function calcNumbers(e){
     if (infinityError) return;
     if (!isOperator(lastInput)){
+        if (lastInput !== "equals"){
         if (currentOperator !== "") storedValue = Number(operate(currentOperator, storedValue, displayValue).toPrecision(DP));
         else storedValue = displayValue;
-
-        console.log(storedValue);
+        
         if (storedValue == Infinity) infinityError = true;
-
+        }
         // this code relies on the first class name of these divs being of the structure e.g divide <- this class MUST be first
         let operatorClass = this.parentElement.className;
         // keep only first class
-        if (operatorClass.indexOf(" ") !== -1) currentOperator = operatorClass.substring(0, operatorClass.indexOf(" "))
+        if (operatorClass.indexOf(" ") !== -1) currentOperator = operatorClass.substring(0, operatorClass.indexOf(" "));
+        
         lastInput = currentOperator;
+        
         document.querySelector(".calc-display").innerText = storedValue;
         displayValue = 0;
 
