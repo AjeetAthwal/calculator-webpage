8c36d599c0f7d12dfe8be45b404f823883e14348
Ajeet <ajeet_athwal@hotmail.co.uk>
2020-11-16 21:35:49 +0000

    fixed bugs on divide by 0

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ let infinityError = false;
 
 const DP = 13;
 
-function getNumberLength(number){
-    return number.toString().replace(".","").length
+function getNumberLength(number) {
+    return number.toString().replace(".", "").length
 }
 
-function operate (operator, a, b){
-    switch(operator){
+function operate(operator, a, b) {
+    switch (operator) {
         case "add":
             return add(Number(a), Number(b));
         case "subtract":
@@ -30,8 +30,8 @@ function operate (operator, a, b){
     }
 }
 
-function operatorToSign(operator){
-    switch(operator){
+function operatorToSign(operator) {
+    switch (operator) {
         case "add":
             return "+";
         case "subtract":
@@ -45,22 +45,23 @@ function operatorToSign(operator){
     }
 }
 
-function isOperator(input){
+function isOperator(input) {
     if (input == "add" || input == "subtract" || input == "multiply" || input == "divide") return true;
     return false;
 }
 
-function addNumberToDisplay(e){
+function addNumberToDisplay(e) {
     if (infinityError) {
-        document.querySelector(".calc-display").innerText = "LOL";
+        document.querySelector(".calc-display").innerText = "ERROR";
+        document.querySelector(".calc-small-display").innerText = "PLS DON'T DIVIDE BY 0";
         return;
     }
     if (lastInput === "equals") return;
 
     const pressedNumber = this.innerText;
 
-    if (lastInput !== ""){
-        if (!isOperator(lastInput)){
+    if (lastInput !== "") {
+        if (!isOperator(lastInput)) {
             if (pressedNumber == "." && displayValue.split("").indexOf(".") != -1) return;
         }
     }
@@ -70,10 +71,10 @@ function addNumberToDisplay(e){
     const displayHtml = document.querySelector(".calc-display");
     if (displayValue == 0) displayValue = pressedNumber;
     else displayValue += pressedNumber.toString();
-    
-    if (getNumberLength(displayValue) > DP){
+
+    if (getNumberLength(displayValue) > DP) {
         displayValue = prevDisplayValue;
-        lastInput  = prevLastInput;
+        lastInput = prevLastInput;
     }
 
     displayHtml.innerText = displayValue;
@@ -81,9 +82,7 @@ function addNumberToDisplay(e){
     else document.querySelector(".calc-small-display").innerText = storedValue + " " + operatorToSign(currentOperator) + " " + displayValue;
 }
 
-document.querySelectorAll(".number-btn").forEach(btn => btn.addEventListener("click", addNumberToDisplay));
-
-function clearDisplay(e){
+function clearDisplay(e) {
     displayValue = 0;
     storedValue = 0;
     currentOperator = "";
@@ -93,44 +92,43 @@ function clearDisplay(e){
     document.querySelector(".calc-display").innerText = displayValue;
 }
 
-document.querySelector(".clear-btn").addEventListener("click", clearDisplay)
-
-function calcNumbers(e){
+function calcNumbers(e) {
     if (infinityError) return;
     if (lastInput === "") return;
-    if (!isOperator(lastInput)){
-        if (lastInput !== "equals"){
-        if (currentOperator !== "") storedValue = Number(operate(currentOperator, storedValue, displayValue).toPrecision(DP));
-        else storedValue = displayValue;
+    if (!isOperator(lastInput)) {
+        if (lastInput !== "equals") {
+            if (currentOperator !== "") storedValue = Number(operate(currentOperator, storedValue, displayValue).toPrecision(DP));
+            else storedValue = displayValue;
 
-        if (storedValue == Infinity) infinityError = true;
+            if (storedValue == Infinity || isNaN(storedValue)) infinityError = true;
         }
         // this code relies on the first class name of these divs being of the structure e.g divide <- this class MUST be first
         let operatorClass = this.parentElement.className;
         // keep only first class
         if (operatorClass.indexOf(" ") !== -1) currentOperator = operatorClass.substring(0, operatorClass.indexOf(" "));
-        
+
         lastInput = currentOperator;
-        
+
         document.querySelector(".calc-display").innerText = storedValue;
         displayValue = 0;
 
         document.querySelector(".calc-small-display").innerText = storedValue + " " + operatorToSign(currentOperator);
 
-        if (infinityError) document.querySelector(".calc-display").innerText = "LOL";
+        if (infinityError) {
+            document.querySelector(".calc-display").innerText = "ERROR";
+            document.querySelector(".calc-small-display").innerText = "PLS DON'T DIVIDE BY 0";
+        }
     }
 }
 
-document.querySelectorAll(".operator-btn").forEach(btn => btn.addEventListener("click", calcNumbers));
-
-function calculateDisplay(e){
+function calculateDisplay(e) {
     if (infinityError) return;
     if (lastInput === "equals") return;
-    if (!isOperator(lastInput)){
+    if (!isOperator(lastInput)) {
         if (currentOperator !== "") storedValue = Number(operate(currentOperator, storedValue, displayValue).toPrecision(DP));
         else storedValue = displayValue;
 
-        if (storedValue == Infinity) infinityError = true;
+        if (storedValue == Infinity || isNaN(storedValue)) infinityError = true;
 
         lastInput = "equals";
         document.querySelector(".calc-display").innerText = storedValue;
@@ -138,8 +136,14 @@ function calculateDisplay(e){
 
         document.querySelector(".calc-small-display").innerText = storedValue;
 
-        if (infinityError) document.querySelector(".calc-display").innerText = "LOL";
+        if (infinityError) {
+            document.querySelector(".calc-display").innerText = "ERROR";
+            document.querySelector(".calc-small-display").innerText = "PLS DON'T DIVIDE BY 0";
+        }
     }
 }
 
+document.querySelectorAll(".number-btn").forEach(btn => btn.addEventListener("click", addNumberToDisplay));
+document.querySelector(".clear-btn").addEventListener("click", clearDisplay)
+document.querySelectorAll(".operator-btn").forEach(btn => btn.addEventListener("click", calcNumbers));
 document.querySelector(".equals-btn").addEventListener("click", calculateDisplay)
